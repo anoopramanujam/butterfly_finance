5747a64a1db3d5004b56ed08ed1758dfcdd24714
Anoop Ramanujam <anoop.r.mundapallil@gmail.com>
2021-06-17 18:38:33 +0530

    clean up the list

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 void main() {
   runApp(MyApp());
@@ -13,17 +14,60 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.lightGreen,
       ),
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Butterfly Finance'),
-        ),
-        body: Center(
-          child: const Text('Hello World',
-              style: const TextStyle(
-                  fontWeight: FontWeight.bold,
-                  color: Colors.deepOrange,
-                  fontSize: 24)),
-        ),
-      ),
+          appBar: AppBar(
+            title: const Text('Butterfly Finance'),
+          ),
+          body: Home()),
     );
   }
+}
+
+/// Home Screen
+class Home extends StatelessWidget {
+  final List<TransactionModel> transactions = [
+    TransactionModel(
+        txnDate: DateTime(2021, 6, 1),
+        amount: 100.00,
+        description: 'Tea with friends'),
+    TransactionModel(
+        txnDate: DateTime(2021, 6, 2),
+        amount: 1200.00,
+        description: 'School Fee'),
+    TransactionModel(
+        txnDate: DateTime(2021, 6, 3),
+        amount: 500.00,
+        description: 'Travel out of town'),
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+        itemCount: transactions.length,
+        itemBuilder: (BuildContext _context, int i) {
+          return _buildRow(transactions[i]);
+        });
+  }
+
+  Widget _buildRow(TransactionModel transaction) {
+    final String txnDate = DateFormat.yMMMMd().format(transaction.txnDate);
+    //final String txnDate = transaction.txnDate.toString();
+    return (ListTile(
+      title: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(transaction.description),
+          Text(transaction.amount.toStringAsFixed(2)),
+        ],
+      ),
+      subtitle: Text(txnDate),
+    ));
+  }
+}
+
+class TransactionModel {
+  TransactionModel(
+      {required this.txnDate, required this.amount, this.description = ''});
+
+  final DateTime txnDate;
+  final String description;
+  final double amount;
 }
